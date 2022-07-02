8d85f6875c984d0fecd077c26d7f3cbf156178cb
El Giorgio <jruanova1987@gmail.com>
2022-07-02 08:59:28 -0300

    responsive

--- js/scroll.js
+++ js/scroll.js
@@ -10,7 +10,7 @@ function scrollFunction() {
     document.body.scrollTop > 20 ||
     document.documentElement.scrollTop > 20
   ) {
-    mybutton.style.display = "block";
+    mybutton.style.display = "flex";
   } else {
     mybutton.style.display = "";
   }
